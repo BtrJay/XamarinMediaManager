1914cc9574868e7b3a7d2038b6e064fb76546806
Jay Sim <jay@builttoroam.com>
2017-11-01 11:23:07 +1100

    Create Xamarin.forms project with MasterDetails

---
+++ MediaMvvmcrossForms/MediaMvvmcrossForms.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaMvvmcrossForms.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new MediaMvvmcrossForms.App());
+        }
+    }
+}
---
+++ MediaMvvmcrossForms/MediaMvvmcrossForms/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MediaMvvmcrossForms
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
